047ee1459f50ce0692056f5bf998d72e0c73338a
Adam Bogdał <adam@bogdal.pl>
2015-12-22 18:51:28 +0100

    Get rid of unnecessary duplication

--- setup.py
+++ setup.py
@@ -25,26 +25,6 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
-    test_args = []
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 class DownloadJSONFiles(Command):
 
     description = 'Download all addresses data from Google i18n API'
