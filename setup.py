3a37930c7f40f245c5105da1cac2c51b6af4fdf6
Patryk Zawadzki <patrys@room-303.com>
2016-12-15 13:54:28 +0100

    2.0.1

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     author_email='hello@mirumee.com',
     description='Address validation helpers for Google\'s i18n address database',
     license='BSD',
-    version='2.0.0',
+    version='2.0.1',
     url='https://github.com/mirumee/google-i18n-address',
     packages=find_packages(),
     include_package_data=True,
