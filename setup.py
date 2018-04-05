d0bd5fcea9fa71974ce4528c1eff7b4cb9605690
Patryk Zawadzki <patrys@room-303.com>
2018-04-05 11:30:47 +0200

    :arrow_up: version bumping to 2.3.0

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     author_email='hello@mirumee.com',
     description='Address validation helpers for Google\'s i18n address database',
     license='BSD',
-    version='2.2.3',
+    version='2.3.0',
     url='https://github.com/mirumee/google-i18n-address',
     packages=find_packages(),
     include_package_data=True,
